4135847913e093297c5c209f6651a19788a579f1
roxxel <64275777+roxxel@users.noreply.github.com>
2022-06-28 11:14:01 +0300

    Add project files.

---
+++ DVBot/Client.cs
@@ -0,0 +1,39 @@
+﻿using dotenv.net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp;
+
+namespace DVBot
+{
+    public class Client
+    {
+        private TelegramClient _client;
+
+        public Client()
+        {
+            var env = DotEnv.Read();
+            _client = new TelegramClient(new
+            (
+                "dv_bot",
+                int.Parse(env["DV_APIID"]),
+                env["DV_APIHASH"],
+                null,
+                env["DV_BOTTOKEN"]
+            ));
+            _client.AuthorizationStateChanged += OnClientAuthorizationStateChanged;
+        }
+
+        private async void OnClientAuthorizationStateChanged(object? sender, TeleSharp.Types.AuthorizationStateChangedEventArgs e)
+        {
+            Console.WriteLine(e.State);
+            if (e.State == TeleSharp.Enums.AuthorizationState.Ready)
+            {
+                await _client.SendTextMessageAsync(chatId: 0, message: "I'm ready!");
+            }
+            await e.Respond(Console.ReadLine());
+        }
+    }
+}
---
+++ DVBot/Program.cs
@@ -0,0 +1,7 @@
+﻿using DVBot;
+using dotenv.net;
+
+DotEnv.Load();
+
+new Client();
+await Task.Delay(-1);
